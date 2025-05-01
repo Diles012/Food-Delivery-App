9c6c925b2f9a9706fb47c50f264063925617a6a5
Dileswar Pradhan <137864999+Diles012@users.noreply.github.com>
2025-05-02 00:48:25 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import validator from "validator";
+import "dotenv/config.js";
 
 
 // Create token
